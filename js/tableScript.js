5650ce346bc31493eb78792103b341422bca441c
Terry_LRG <lnuiweihao@163.com>
2024-10-25 18:41:19 +0800

    初次提交html练习文件

---
+++ js/tableScript.js
@@ -0,0 +1,10 @@
+
+let table = document.getElementById("grid");
+let addBtn = document.getElementById("addBtn");
+addBtn.addEventListener("click",AddBtnClickEvent);
+console.log(addBtn);
+
+function AddBtnClickEvent()
+{
+    alert("test12345");
+}
